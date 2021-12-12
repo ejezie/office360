33980b3a39ea0050abcf23be1efa1fab7416d8bf
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2021-12-12 17:53:20 +0100

    responsive navbar

---
+++ src/component/Drawer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+
+function Drawer() {
+    return (
+      <div className="h">
+          <h1><a href="https://google.com">hello</a></h1>
+      </div>
+    );
+}
+
+export default Drawer
---
+++ src/component/Navbar.jsx
@@ -0,0 +1,75 @@
+import React, { useState, useRef } from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/mlogo.png";
+
+function Navbar() {
+  const [rotateBar, setRotatebar] = useState(false);
+
+  const select1 = useRef(null);
+  const select2 = useRef(null);
+  const select3 = useRef(null);
+
+  const handleBar = () => {
+    setRotatebar(!rotateBar);
+  };
+  const handleLinkClick1 = () => {
+    const div1 = select1.current;
+    const div2 = select2.current;
+    const div3 = select3.current;
+    div1.className = "select";
+    div2.className = "";
+    div3.className = "";
+  };
+  const handleLinkClick2 = () => {
+    const div1 = select1.current;
+    const div2 = select2.current;
+    const div3 = select3.current;
+    div1.className = "";
+    div2.className = "select";
+    div3.className = "";
+  };
+  const handleLinkClick3 = () => {
+    const div1 = select1.current;
+    const div2 = select2.current;
+    const div3 = select3.current;
+    div1.className = "";
+    div2.className = "";
+    div3.className = "select";
+  };
+
+  return (
+    <div className="navbar">
+      <div className="wrap">
+        <div className="menu" onClick={handleBar}>
+          <div className={`bar ${rotateBar && "one"}`}></div>
+          <div className={`bar ${rotateBar && "two"}`}></div>
+          {rotateBar && "close"}
+        </div>
+        <img className="logo" src={logo} alt="logo" />
+        <div className="links">
+          <Link onClick={handleLinkClick1} to={"/"}>
+            Home <div ref={select1} className="select"></div>
+          </Link>
+          <Link onClick={handleLinkClick2} to={"/"}>
+            Office Space <div ref={select2} className=""></div>
+          </Link>
+          <Link onClick={handleLinkClick3} to={"/"}>
+            About <div ref={select3} className=""></div>
+          </Link>
+        </div>
+        <a className="call" href="tel:08142074224">
+          <i class="fas fa-headset"></i>
+        </a>
+      </div>
+      <div className={`drawer ${rotateBar && "show"}`}>
+        <div className="links">
+          <Link to={"/"}>Home</Link>
+          <Link to={"/"}>Office Spaces</Link>
+          <Link to={"/"}>About</Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
